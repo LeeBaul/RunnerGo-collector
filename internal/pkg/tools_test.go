062bf5f0a28662ca53d9a9269d85f3cafde1df4a
guosongsong <guosongsong@apipost.cn>
2022-11-15 17:44:34 +0800

    优化rps，srps

--- internal/pkg/tools_test.go
+++ internal/pkg/tools_test.go
@@ -1,7 +1,12 @@
 package pkg
 
-import "testing"
+import (
+	"fmt"
+	"runtime"
+	"testing"
+)
 
 func TestPost(t *testing.T) {
-	Post("https://dev-tools-env-test.apipost.cn/api/demo/news_list?mobile=18289454846&theme_news=国际新闻&page=1&pageSize=20", "")
+	//Post("https://dev-tools-env-test.apipost.cn/api/demo/news_list?mobile=18289454846&theme_news=国际新闻&page=1&pageSize=20", "")
+	fmt.Println(runtime.NumCPU())
 }
